9a39ad564d774cb26bc7577fe97ee7a7e0e89965
nadavw1312 <nadavw1312@gmail.com>
2023-11-26 19:34:19 +0200

    - update

--- registration/index.mjs
+++ registration/index.mjs
@@ -10,31 +10,3 @@ export const handler = async (event) => {
   };
   return response;
 };
-
-// (async () => {
-//   await handler({
-//     body: {
-//       firstName: "bdika",
-//       lastName: "bdika",
-//       israeliId: "999999999",
-//       phoneNumber: "+9720544598264",
-//       password: "12345678aA@",
-//     }.toString(),
-//   });
-// })();
-
-// import express from "express";
-// const app = express();
-// const PORT = 3000;
-
-// app.use(express.json());
-
-// app.post("/user", async (req, res) => {
-//   const response = await register(req.body);
-//   res.status(response.statusCode).send(response.body);
-// });
-
-// app.listen(PORT, (error) => {
-//   if (!error) console.log("Server is Successfully Running,  and App is listening on port " + PORT);
-//   else console.log("Error occurred, server can't start", error);
-// });
--- registration/local-test.mjs
+++ registration/local-test.mjs
@@ -1,5 +1,13 @@
 import { handler } from "./index.mjs";
 
 (async () => {
-  await handler({});
+  await handler({
+    body: {
+      firstName: "bdika",
+      lastName: "bdika",
+      israeliId: "999999999",
+      phoneNumber: "+9720544598232",
+      password: "12345678aA@",
+    }.toString(),
+  });
 })();
